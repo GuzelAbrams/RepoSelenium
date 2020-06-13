0220e5b43eed678d06fcd5c0585a297285ded296
Guzel Abrams <guzelabrams@Guzels-MBP.home>
2020-06-13 13:15:35 -0400

    First selenium code

---
+++ src/test/java/FirstPkg/First.java
@@ -0,0 +1,12 @@
+package FirstPkg;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class First {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.get(“https://www.amazom.com”);
+
+    }
+}
